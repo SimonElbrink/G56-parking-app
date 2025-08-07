e75c2ba6e9f703b7054968e850f395cf963bb340
Simon Elbrink <45754982+SimonElbrink@users.noreply.github.com>
2025-08-07 15:06:19 +0200

    ParkingApp - Register user and Reserve spot.

--- src/main/java/se/lexicon/ParkingApp.java
+++ src/main/java/se/lexicon/ParkingApp.java
@@ -1,20 +1,31 @@
 package se.lexicon;
 
+import se.lexicon.dao.CustomerDao;
 import se.lexicon.dao.ParkingSpotDao;
+import se.lexicon.dao.ReservationDao;
+import se.lexicon.dao.impl.CustomerDaoImpl;
 import se.lexicon.dao.impl.ParkingSpotDaoImpl;
+import se.lexicon.dao.impl.ReservationDaoImpl;
+import se.lexicon.model.Customer;
 import se.lexicon.model.ParkingSpot;
+import se.lexicon.model.Reservation;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.Scanner;
 
 public class ParkingApp {
 
     private ParkingSpotDao parkingSpotDao;
+    private CustomerDao customerDao;
+    private ReservationDao reservationDao;
 
 
     //The app is starting
     public ParkingApp() {
         parkingSpotDao = new ParkingSpotDaoImpl();
+        customerDao = new CustomerDaoImpl();
+        reservationDao = new ReservationDaoImpl();
 
         initializeParkingSpots(50005);
     }
@@ -26,6 +37,7 @@ public class ParkingApp {
 
         while (running) {
             System.out.println("""
+                    
                     1. Register Customer
                     2. Display Parking Spots
                     3. Reserve a Parking Spot
@@ -58,9 +70,66 @@ public class ParkingApp {
     }
 
     private void reserveParkingSpot() {
+        Scanner scanner = new Scanner(System.in);
+
+        //Is there Available Spots?
+        if (parkingSpotDao.findAvailableSpots().isEmpty()) {
+            System.out.println(ConsoleColors.RED + "No Available parking spots." + ConsoleColors.RESET);
+            return;
+        }
+
+
+        // What spot is selected?
+        System.out.println("Enter Spot ID: ");
+        String spotID = scanner.nextLine();
+
+        Optional<ParkingSpot> foundSpot = parkingSpotDao.findBySpotNumber(Integer.parseInt(spotID));
+        if (foundSpot.isEmpty()) {
+            System.out.println(ConsoleColors.RED + "Invalid Spot ID" + ConsoleColors.RESET);
+            return;
+        }
+
+        if (foundSpot.get().isOccupied()) {
+            System.out.println(ConsoleColors.RED + "Spot is Occupied" + ConsoleColors.RESET);
+            return;
+        }
+
+        // Do we have customer?
+        System.out.println("Enter Customer ID: ");
+        String customerID = scanner.nextLine();
+
+        Optional<Customer> foundCustomer = customerDao.findById(Integer.parseInt(customerID));
+        if (foundCustomer.isEmpty()) {
+            System.out.println(ConsoleColors.RED + "Invalid Customer ID" + ConsoleColors.RESET);
+            return;
+        }
+
+        // How long?
+        System.out.println("Enter Reservation hours: ");
+        String hours = scanner.nextLine();
+
+        //Create Reservation
+        Reservation reservation = new Reservation(foundSpot.get(), foundCustomer.get(), Integer.parseInt(hours));
+
+        //Store the Reservation
+        Reservation savedReservation = reservationDao.create(reservation);
+
+        //Update occupy
+        foundSpot.get().occupy();
+        parkingSpotDao.update(foundSpot.get());
+
+
+        //Display if it was successful
+        System.out.println(ConsoleColors.GREEN + "Reservation Created: " + savedReservation + ConsoleColors.RESET);
+
     }
 
     private void displayParkingSpots() {
+
+        if (parkingSpotDao.findAvailableSpots().isEmpty()) {
+            System.out.println(ConsoleColors.RED + "No Available parking spots." + ConsoleColors.RESET);
+            return;
+        }
 
         List<ParkingSpot> spots = parkingSpotDao.findAll();
 
@@ -81,6 +150,21 @@ public class ParkingApp {
     }
 
     private void registerCustomer() {
+
+        Scanner scanner = new Scanner(System.in);
+        // Request input for creation
+        System.out.println("Enter customer name: ");
+        String name = scanner.nextLine();
+        System.out.println("Enter phone number: ");
+        String phone = scanner.nextLine();
+        System.out.println("Enter vehicle plate number:");
+        String plateNumber = scanner.nextLine();
+
+        //Store customer
+        Customer createdCustomer = customerDao.create(new Customer(name, phone, plateNumber));
+
+        System.out.println(ConsoleColors.GREEN + "Customer registered with ID: " + createdCustomer.getId() + ConsoleColors.RESET);
+
 
     }
 
